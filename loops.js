5fbbdcec50c9426f96434b9e83c085fd6ceac1da
Dylan Heinrich <harly13@live.com>
2019-02-16 16:08:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function forLoop(array) {
-  for(var i = 1; i < 25; i++) {
-    array = [...array, `I am ${i} strange loop.`]
+  for(var i = 0; i < 25; i++) {
+    if(i === 0){
+      array.push('I am 1 strange loop.')
+    }
+    else{
+      array.push(`I am ${i} strange loop.`)
+    }
   }
   return array
 }
